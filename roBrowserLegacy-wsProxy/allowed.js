03bdcb86a89aa892e5334ac0cba7add91e0b8e46
Alvin Chong <alvinchong.sf@gmail.com>
2026-01-19 23:16:35 -0800

    add explicit ports for testing

--- roBrowserLegacy-wsProxy/allowed.js
+++ roBrowserLegacy-wsProxy/allowed.js
@@ -2,7 +2,7 @@
 // If empty, will be able to redirect everywhere
 // Be aware: can be used for DDOS or forge evil request on other host
 module.exports = [
-	//"83.xxx.x.1:6900",
-	//"83.xxx.x.1:5121",
-	//"83.xxx.x.1:6121"
+    '127.0.0.1:6900',  // login
+    '127.0.0.1:6121',  // char
+    '127.0.0.1:5121'   // map
 ];
