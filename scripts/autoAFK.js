2a8438134fae66fbebc3a0ebdd55238d9881c3ca
Alvin Chong <alvinchong.sf@gmail.com>
2026-08-06 18:36:51 -0700

    added logs

--- scripts/autoAFK.js
+++ scripts/autoAFK.js
@@ -2,6 +2,7 @@ const opts = { key: 'Enter', code: 'Enter', keyCode: 13, which: 13, bubbles: tru
 const message = "@afk";
 const inputDiv = '[contenteditable="true"]';
 const keydown = 'keydown'
+let interval;
 
 const element = document.querySelector(inputDiv);
 element.focus();
@@ -11,13 +12,17 @@ setTimeout(() => {
     document.dispatchEvent(new KeyboardEvent(keydown, opts));
 }, 1000)
 
-const interval = setInterval(() => {
+interval = setInterval(() => {
+    console.log('AFK trigger firing at', new Date().toLocaleTimeString());
     const element = document.querySelector(inputDiv);
     element.focus();
     element.textContent = message;
+    console.log('AFK trigger2');
 
     setTimeout(() => {
         document.dispatchEvent(new KeyboardEvent(keydown, opts));
+        console.log('AFK trigger3');
     }, 1000)
 }, 930000)
 
+console.log('AFK script started, interval ID:', interval);
